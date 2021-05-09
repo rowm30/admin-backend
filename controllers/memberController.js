067c97d391631b4d3154adba77423172fba949c2
rowm30 <rowm30@gmail.com>
2021-05-09 15:20:44 +0530

    BackEndComplete\

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var router = express.Router();
+var ObjectId = require('mongoose').Types.ObjectId;
 
 var { technojamMembers } = require('../models/technojamMembers')
 
@@ -9,6 +10,18 @@ router.get('/' , (req,res) => {
         else { console.log('Error in Retriving Members: ' + JSON.stringify(err, undefined, 2));}
     });
 })
+
+router.get('/:id', (req,res)=>{
+    if (!ObjectId.isValid(req.params.id)) {
+        return res.status(400).send(`No record with given id:  ${req.params.id}`)
+    }
+
+    technojamMembers.findById(req.params.id, (err,doc)=>{
+        if(!err){res.send(doc);}
+        else{console.log('Error in retriving employee:' + JSON.stringify(err, undefined, 2));}
+    });
+})
+
 
 router.post('/', (req, res)=>{
     var mem = new technojamMembers({
@@ -22,5 +35,30 @@ router.post('/', (req, res)=>{
         else{console.log('Error in Employee save: ' + JSON.stringify(err,undefined,2));}
     })
 })
+
+router.put('/:id',(req,res)=>{
+    if (!ObjectId.isValid(req.params.id)) {
+        return res.status(400).send(`No record with given id:  ${req.params.id}`)
+    }
+
+    var mem = {
+        name: req.body.name,
+        batch: req.body.batch,
+        role: req.body.role,
+        votes: req.body.votes
+    }
+
+    technojamMembers.findByIdAndUpdate(req.params.id, {$set:emp},{new: true},(err,doc)=>{
+        if(!err){res.send(doc);}
+        else{console.log('Error in employee update: ' + JSON.stringify(err,undefined,2));}
+    })
+
+})
+
+router.delete('/:id', (req,res)=>{
+    if(!err){res.send(doc);}
+    else{console.log('Error in employee delete: ' + JSON.stringify(err,undefined,2));}
+})
+
 
 module.exports = router;
