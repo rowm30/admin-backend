c234df888030d26394eb4b0197bebe9ce649ad04
rowm30 <rowm30@gmail.com>
2021-05-15 22:19:30 +0530

    FrontEndWorking

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const { mongoose } = require('./db.js');
 var memberController = require('./controllers/memberController.js')
 
 var app = express();
 app.use(bodyParser.json());
+app.use(cors({origin:'http://localhost:4200'}))
 
 app.listen(3000, () => console.log('Server strated at port: 3000'));
 
